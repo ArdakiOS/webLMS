1bfe24b41a2875048e39e300a460a5b15602af59
Ardak <luxr2ge@gmail.com>
2023-01-18 14:15:40 +0600

    Initial Commit

---
+++ webLMS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  webLMS
+//
+//  Created by Ardak Tursunbayev on 18.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ webLMS/webLMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  webLMSApp.swift
+//  webLMS
+//
+//  Created by Ardak Tursunbayev on 18.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct webLMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
